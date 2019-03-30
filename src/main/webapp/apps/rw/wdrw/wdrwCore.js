5827aaec5e7e5fb59e37ed036be1a35a45f757c4
liuchonghua <a397513182@qq.com>
2019-03-30 11:54:10 +0800

    任务接口待完成

--- src/main/webapp/apps/rw/wdrw/wdrwCore.js
+++ src/main/webapp/apps/rw/wdrw/wdrwCore.js
@@ -1,76 +1,100 @@
 var wdrw = new Vue({
-	el: '#wdrwapp',
-	data: {
-		rwlist: [],
-		rw: {},
-		rwzt:{
-			type:'待审批'
-		},
-		msg: {
-			mess: ''
-		}
-	},
-	methods: {
-		init: function() {
-			if (wdrw===undefined){
-				axios.post('/sp/spgl/findMyWFQ',{type:'待审批'}).then(function(result) {
-					wdrw.splist = result.data;
-				}).catch(function(err) {
-					console.log(err);
-				});
-			}else {
-				axios.post('/sp/spgl/findMyWFQ',wdrw.spzt).then(function(result) {
-					wdrw.splist = result.data;
-				}).catch(function(err) {
-					console.log(err);
-				});
-			}
-		},
-		summary:function(nr){
-			if (nr.length>10){
-				return nr.substr(0,10)+"...";
-			}else {
-				return nr;
-			}
-		},
-		//详情
-		detail: function(item) {
-			$('#xqform').modal('show');
-			if (item.splx==='出差') {
-				$(".chuchai").show();
-			}else {
-				$(".chuchai").hide();
-			}
-			if (item.type==='待审批') {
-				$("#cancel").show();
-			}else {
-				$("#cancel").hide();
-			}
-			wdrw.sp = item;
-		},
-		format: function(cjsj) {
-			return Global.Fun.Format(cjsj, 'yyyy-MM-dd HH:mm:ss');
-		},
-		cancel:function () {
-			axios.post('/sp/spgl/cancel', wdrw.sp).then(function (result) {
-				$('#xqform').modal('hide');
-				wdrw.msg.mess = result.data.mess;
-				$('#msg').modal('show');
-				wdrw.init();
-			}).catch(function (err) {
-				console.log(err);
-			});
-		}
-	},
-	mounted: function() {
-		this.init();
-		$('#xqform').on('hide.bs.modal',
-			function() {
-				wdrw.sp = {};
-				wdrw.init();
-			}
-		);
+    el: '#wdrwapp',
+    data: {
+        rwlist: [],
+        rw: {},
+        rwzt: {
+            type: ''
+        },
+        msg: {
+            mess: ''
+        }
+    },
+    methods: {
+        init: function () {
+            if (wdrw === undefined) {
+                axios.post('/rw/rwgl/findMyRw', {type: ''}).then(function (result) {
+                    wdrw.rwlist = result.data;
+                }).catch(function (err) {
+                    console.log(err);
+                });
+            } else {
+                axios.post('/rw/rwgl/findMyRw', wdrw.rwzt).then(function (result) {
+                    wdrw.rwlist = result.data;
+                }).catch(function (err) {
+                    console.log(err);
+                });
+            }
+        },
+        summary: function (nr) {
+            if (nr.length > 10) {
+                return nr.substr(0, 10) + "...";
+            } else {
+                return nr;
+            }
+        },
+        //详情
+        detail: function (item) {
+            if (item.type==='未开始') {
+                $('#processing').show();
+                $('#completed').hide();
+            }else if (item.type==='进行中') {
+                $('#processing').hide();
+                $('#completed').show();
+            }else {
+                $('#processing').hide();
+                $('#completed').hide();
+            }
+            $('#xqform').modal('show');
+            wdrw.rw = item;
+        },
+        format: function (cjsj) {
+            if (cjsj!=null&&cjsj!==''){
+                return Global.Fun.Format(cjsj, 'yyyy-MM-dd HH:mm:ss');
+            } else {
+                return '--'
+            }
+        },
+        //过期状态
+        expiredStatus: function (time) {
+            console.log(time);
+            console.log("time");
+            console.log(new Date());
+            return time;
+        },
+        //已完成
+        completed: function () {
+            axios.post('/rw/rwgl/fulfill', wdrw.rw).then(function (result) {
+                $('#xqform').modal('hide');
+                wdrw.msg.mess = result.data.mess;
+                $('#msg').modal('show');
+                wdrw.init();
+            }).catch(function (err) {
+                console.log(err);
+            });
+        },
+        //进行中
+        processing: function () {
+            axios.post('/rw/rwgl/hasNotStarted', wdrw.rw).then(function (result) {
+                $('#xqform').modal('hide');
+                wdrw.msg.mess = result.data.mess;
+                $('#msg').modal('show');
+                wdrw.init();
+            }).catch(function (err) {
+                console.log(err);
+            });
+        }
 
-	}
+    },
+    mounted: function () {
+        this.init();
+        $('#xqform').on('hide.bs.modal',
+            function () {
+                wdrw.rw = {};
+                wdrw.init();
+            }
+        );
+
+    }
 });
 	
